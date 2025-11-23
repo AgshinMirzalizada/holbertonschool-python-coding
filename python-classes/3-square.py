f1f11360389092fc0d401a07a674d2d9ec231240
AgshinMirzalizada <aqsinmirzalizada@gmail.com>
2025-11-23 14:31:07 -0500

    .

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,14 +1,22 @@
 #!/usr/bin/python3
+"""This is documentation of the class."""
+
+
 class Square:
+    """This is a Square class."""
+
     def __init__(self, size=0):
-        self.size = size
+        """Initialize the square with optional size."""
+        self.size = size  # setter is used for validation
 
     @property
     def size(self):
+        """Retrieve size."""
         return self.__size
 
     @size.setter
     def size(self, value):
+        """Set size with validation."""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
@@ -16,4 +24,5 @@ class Square:
         self.__size = value
 
     def area(self):
-        return self.__size * self.__size
+        """Return area of the square."""
+        return self.__size ** 2
